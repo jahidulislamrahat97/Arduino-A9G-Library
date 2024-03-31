8031671ea7b6e0abe2ead5510ab4ea7f920d4738
jahidulislamrahat97 <jahidulislamrahat97@gmail.com>
2024-03-31 21:26:49 +0600

    IMEI CCID CSQ Added

--- examples/CSQ_IMEI_CCID/CSQ_IMEI_CCID.ino
+++ examples/CSQ_IMEI_CCID/CSQ_IMEI_CCID.ino
@@ -1,3 +1,12 @@
+/*********************************************************************************
+   If this code works, it was written by Jahidul Islam Rahat.
+   If not, I don't know who wrote it.
+   :) xD
+
+   Author: Jahidul Islam Rahat.
+   Date: 25 March 2024.
+*********************************************************************************/
+
 #include <Arduino.h>
 #include <A9G.h>
 
@@ -7,58 +16,56 @@ GSM gsm(1);
 const int gsm_pin = 15;
 
 
-void eventDispatch(A9G_Event_t *event)
-{
-  switch (event->id)
-  {
-  case EVENT_MQTTPUBLISH:
-    Serial.print("Topic: ");
-    Serial.println(event->topic);
-    Serial.printf("message: %s\n", event->message);
-    break;
+void eventDispatch(A9G_Event_t *event) {
+  switch (event->id) {
+    case EVENT_MQTTPUBLISH:
+      Serial.print("Topic: ");
+      Serial.println(event->topic);
+      Serial.printf("message: %s\n", event->message);
+      break;
 
-  case EVENT_NEW_SMS_RECEIVED:
-    Serial.print("Number: ");
-    Serial.println(event->number);
-    Serial.print("Message: ");
-    Serial.println(event->message);
-    Serial.print("Date & Time: ");
-    break;
+    case EVENT_NEW_SMS_RECEIVED:
+      Serial.print("Number: ");
+      Serial.println(event->number);
+      Serial.print("Message: ");
+      Serial.println(event->message);
+      Serial.print("Date & Time: ");
+      break;
 
-  case EVENT_CSQ:
-    Serial.print("CSQ: ");
-    Serial.println(event->param1);
-    break;
+    case EVENT_CSQ:
+      Serial.print("CSQ: ");
+      Serial.println(event->param1);
+      break;
 
-  case EVENT_IMEI:
-    Serial.print("IMEI: ");
-    Serial.println(event->param2);
-    break;
+    case EVENT_IMEI:
+      Serial.print("IMEI: ");
+      Serial.println(event->param2);
+      break;
 
-  case EVENT_CCID:
-    Serial.print("CCID: ");
-    Serial.println(event->param2);
-    break;
+    case EVENT_CCID:
+      Serial.print("CCID: ");
+      Serial.println(event->param2);
+      break;
 
-  case EVENT_CME:
-    Serial.print("CME ERROR Message:");
-    gsm.errorPrintCME(event->error);
-    break;
+    case EVENT_CME:
+      Serial.print("CME ERROR Message:");
+      gsm.errorPrintCME(event->error);
+      break;
 
-  case EVENT_CMS:
-    Serial.print("CMS ERROR Message:");
-    gsm.errorPrintCMS(event->error);
-    break;
+    case EVENT_CMS:
+      Serial.print("CMS ERROR Message:");
+      gsm.errorPrintCMS(event->error);
+      break;
 
-  default:
-    break;
+    default:
+      break;
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
+  // GSM power reset would be best for specially in bangladesh 2g/3g network. it's not mandatory but try to use it.
   pinMode(gsm_pin, OUTPUT);
   digitalWrite(gsm_pin, HIGH);
   delay(4000);
@@ -68,17 +75,18 @@ void setup()
 
   A9G.begin(115200);
   gsm.init(&A9G);
-
   gsm.EventDispatch(eventDispatch);
 
-  if (gsm.waitForReady())
-  {
+  //Don not use this function if you are not aware of this funciton. it's fully blocking code but it's very usefull.
+  if (gsm.waitForReady()) {
     Serial.println("A9G Ready");
   }
-  else
-  {
-    Serial.println("A9G Not Ready");
-  }
+
+  // if (gsm.bIsReady()) {
+  //   Serial.println("A9G Ready");
+  // } else {
+  //   Serial.println("A9G Not Ready");
+  // }
 
 
   delay(3000);
@@ -94,9 +102,9 @@ void setup()
   gsm.ReadCCID();
 }
 
-void loop()
-{
+void loop() {
   gsm.executeCallback();
 
   delay(15);
 }
+
